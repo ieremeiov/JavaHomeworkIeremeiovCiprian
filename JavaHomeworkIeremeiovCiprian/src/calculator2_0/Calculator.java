2fa9aa2b0e98a427b0c854d5fd4c5d666aea5a0a
Cip <Cip@Ciprian>
2017-06-18 20:40:51 +0300

    Added the option to reset the calculator. Modified the precision to 2 digits.

--- JavaHomeworkIeremeiovCiprian/src/calculator2_0/Calculator.java
+++ JavaHomeworkIeremeiovCiprian/src/calculator2_0/Calculator.java
@@ -1,6 +1,6 @@
 /*
- *   Calculator 2.0 can perform basic operations on integers. You can add, substract, multiply and divide numbers. 
- *   It assumes you input the right numbers (as Integer) in the right order and press '=' at the end. 
+ *   Calculator 2.0 can perform basic operations on Integers. You can add, substract, multiply and divide numbers. 
+ *   It assumes you input the right numbers (as Integers) in the right order and press '=' at the end. It will output a result with a precision of 2 digits.
  *
  */
 package calculator2_0;
