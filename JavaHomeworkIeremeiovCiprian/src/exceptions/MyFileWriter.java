211b122663ab845e3943ca588df4e03b851ded7f
Cip <Cip@Ciprian>
2017-08-04 11:03:40 +0300

    Writer added

--- JavaHomeworkIeremeiovCiprian/src/exceptions/MyFileWriter.java
+++ JavaHomeworkIeremeiovCiprian/src/exceptions/MyFileWriter.java
@@ -32,18 +32,18 @@ public class MyFileWriter {
     // can throw exceptionsother than IOException depending on what other methods are being invoked inside the try block 
     // although bad practice, I've chosen to throw the exception further so that the exception will be handled in main() in this case
     // it will only throw other exceptions if you manually add more dangerous methods inside the body of writeFile() 
-    public static void writeFile(String outputFile, String text) throws Exception {
+    public static void writeFile(String outputFile, String textToWrite) throws Exception {
 
         FileWriter file = null;
         PrintWriter writer = null;
 
         try {
 
-            file = new FileWriter(outputFile); // can throw IOException
+            file = new FileWriter(outputFile); // can throw IOException if file cannot be created
 
             writer = new PrintWriter(file);
 
-            writer.println(text);
+            writer.println(textToWrite);
             writer.close();
 
         } catch (IOException e) {
